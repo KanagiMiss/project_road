fd9eb7822f82b95e9ddab4007f4d6d3942a352d4
Zhouyu Meng <misskanagi@gmail.com>
2018-01-27 17:30:53 +0800

    good generator

---
+++ Assets/Scripts/GradientBlack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GradientBlack : MonoBehaviour {
+	public Mesh mesh;
+	public Material material;
+	// Use this for initialization
+	void Start () {
+		/*
+		MeshFilter viewedModelFilter = (MeshFilter)GetComponent("MeshFilter");
+		Mesh mesh = viewedModelFilter.mesh;
+		Color[] colors = new Color[mesh.vertices.Length];
+		Color BottomColor = Color.red;
+		Color TopColor = Color.blue;
+		colors[0] = BottomColor;
+		colors[1] = TopColor;
+		colors[2] = BottomColor;
+		colors[3] = TopColor;
+		mesh.colors = colors;*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// will make the mesh appear in the scene at origin position
+		Graphics.DrawMesh(mesh, Vector3.zero, Quaternion.identity, material, 0);
+	}
+}
