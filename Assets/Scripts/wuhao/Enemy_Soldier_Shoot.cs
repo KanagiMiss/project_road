75722f661922a881e7ef3c7623d732b28484142e
hackhao <hh@smail.nju.edu.cn>
2018-01-27 21:03:03 +0800

    update

---
+++ Assets/Scripts/wuhao/Enemy_Soldier_Shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Soldier_Shoot1 : Enemy {
+	public GameObject prefabBullet;
+	public float rateScale = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+
+
+	//
+	public override void Move(){
+		Vector2 tempPos = transform.position;
+		//对tempPos进行更新操作
+		if (this.direction == EnemyDirection.LEFT_TO_RIGHT) {
+			tempPos.x += Time.deltaTime * this.speed;
+		} else {
+			tempPos.x -= Time.deltaTime * this.speed;
+		}
+	}
+		
+}
