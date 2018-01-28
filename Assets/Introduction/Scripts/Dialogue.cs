3661e7811601499f41f94c0f73abae16734f831e
hackhao <hh@smail.nju.edu.cn>
2018-01-28 14:13:11 +0800

    upload introduction

---
+++ Assets/Introduction/Scripts/Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GOAT{
+
+	[System.Serializable]
+	public class Dialogue {
+
+		public string[] title;
+
+		public string[] sentences;
+
+	}
+}
